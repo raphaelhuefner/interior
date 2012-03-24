71f8a88d6b45dd06198e123c7b848625b24a5426
Raphael Huefner <raphael@affinitybridge.com>
2012-03-24 14:27:08 -0700

    talk to cam

--- assets/js/decorator/camera.js
+++ assets/js/decorator/camera.js
@@ -1,5 +1,5 @@
 function dump_pic(data) {
-    $('#gotten-pictures').append('<p><img src="' + data + '" />' + data + '</p>');
+    $('#gotten-pictures').append('<p><img src="' + data + '" /><br />' + data + '</p>');
 }
 
 function fail(msg) {
@@ -8,6 +8,6 @@ function fail(msg) {
 
 function show_pic() {
     navigator.camera.getPicture(dump_pic, fail, {
-        quality : 50, destinationType : Camera.DestinationType.DATA_URL
+        quality : 50, destinationType : Camera.DestinationType.FILE_URI
     });
 }
