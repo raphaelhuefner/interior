ac5003da59a2d19e7fe188b7fc28f2354bb4615d
Raphael Huefner <raphael@affinitybridge.com>
2012-03-24 15:58:09 -0700

    talk to tineye, first try

--- assets/js/decorator/camera.js
+++ assets/js/decorator/camera.js
@@ -12,7 +12,7 @@ function getPic() {
   navigator.camera.getPicture(dump_pic, fail, {
     quality : 50
     , destinationType : Camera.DestinationType.FILE_URI
-//    , targetWidth: 200
-//    , targetHeight: 200
+    , targetWidth: 200
+    , targetHeight: 200
   });
 }
